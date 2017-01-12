916e41ce99a622de058a027392096079b13b1efa
cheekbyname <zenspideruk@googlemail.com>
2017-01-12 09:54:47 +0000

    Tile Page UI work

--- app/pages/tile.page/tile.page.ts
+++ app/pages/tile.page/tile.page.ts
@@ -10,62 +10,66 @@ export class TilePage {
 	}
 
 	tileGroups: TileGroup[] = [
-		new TileGroup(0, "The Tasks - Do They Involve"),
-		new TileGroup(1, "Individual Capability - Does the Job"),
-		new TileGroup(2, "The Loads - Are They"),
-		new TileGroup(3, "The Working Envrionment - Are There"),
-		new TileGroup(4, "Other Factors")
-	];
-
-	tileItems: TileItem[] = [
-		new TileItem(this.tileGroups[0], "Holding loads away from trunk?"),
-		new TileItem(this.tileGroups[0], "Twisting?"),
-		new TileItem(this.tileGroups[0], "Stooping?"),
-		new TileItem(this.tileGroups[0], "Reaching Upwards?"),
-		new TileItem(this.tileGroups[0], "Large vertical movement?"),
-		new TileItem(this.tileGroups[0], "Long carrying distances?"),
-		new TileItem(this.tileGroups[0], "Strenuous pushing or pulling?"),
-		new TileItem(this.tileGroups[0], "Unpredicatable movement of loads?"),
-		new TileItem(this.tileGroups[0], "Repetitive handling?"),
-		new TileItem(this.tileGroups[0], "Insufficient rest or recovery?"),
-		new TileItem(this.tileGroups[0], "A work rate imposed by a process?"),
-		new TileItem(this.tileGroups[1], "Requrie unusual capability?"),
-		new TileItem(this.tileGroups[1], "Hazard those with a health problem?"),
-		new TileItem(this.tileGroups[1], "Hazard those who are pregnant?"),
-		new TileItem(this.tileGroups[1], "Call for special information/training?"),
-		new TileItem(this.tileGroups[2], "Heavy?"),
-		new TileItem(this.tileGroups[2], "Bulky/Unwieldy?"),
-		new TileItem(this.tileGroups[2], "Difficult to grasp?"),
-		new TileItem(this.tileGroups[2], "Unstable/Unpredicatable?"),
-		new TileItem(this.tileGroups[2], "Constraints on posture?"),
-		new TileItem(this.tileGroups[3], "Poor floors?"),
-		new TileItem(this.tileGroups[3], "Variations in levels?"),
-		new TileItem(this.tileGroups[3], "Hot/cold/humid conditions?"),
-		new TileItem(this.tileGroups[3], "Strong air movements?"),
-		new TileItem(this.tileGroups[3], "Poor lighting conditions?"),
-		new TileItem(this.tileGroups[4], "Is movement or posture hindered by clothing or personal protective equipment?")
+		new TileGroup(0, "Task", "The Tasks - Do They Involve:", [
+			new TileItem("Holding loads away from trunk?"),
+			new TileItem("Twisting?"),
+			new TileItem("Stooping?"),
+			new TileItem("Reaching Upwards?"),
+			new TileItem("Large vertical movement?"),
+			new TileItem("Long carrying distances?"),
+			new TileItem("Strenuous pushing or pulling?"),
+			new TileItem("Unpredicatable movement of loads?"),
+			new TileItem("Repetitive handling?"),
+			new TileItem("Insufficient rest or recovery?"),
+			new TileItem("A work rate imposed by a process?")
+		]),
+		new TileGroup(1, "Individual", "Individual Capability - Does the Job:", [
+			new TileItem("Requrie unusual capability?"),
+			new TileItem("Hazard those with a health problem?"),
+			new TileItem("Hazard those who are pregnant?"),
+			new TileItem("Call for special information/training?"),
+		]),
+		new TileGroup(2, "Load", "The Loads - Are They:", [
+			new TileItem("Heavy?"),
+			new TileItem("Bulky/Unwieldy?"),
+			new TileItem("Difficult to grasp?"),
+			new TileItem( "Unstable/Unpredicatable?"),
+			new TileItem("Constraints on posture?"),
+		]),
+		new TileGroup(3, "Environment", "The Working Envrionment - Are There:", [
+			new TileItem("Poor floors?"),
+			new TileItem("Variations in levels?"),
+			new TileItem("Hot/cold/humid conditions?"),
+			new TileItem("Strong air movements?"),
+			new TileItem("Poor lighting conditions?"),
+		]),
+		new TileGroup(4, "Other", "Other Factors:", [
+			new TileItem("Is movement or posture hindered by clothing or personal protective equipment?")
+		])
 	];
 
 }
 
 class TileItem {
-	tileGroup: TileGroup;
 	task: string;
 	hazard: boolean;
 	remedialAction: string;
 
-	constructor(group: TileGroup, task: string) {
-		this.tileGroup =group;
+	constructor(task: string) {
 		this.task = task;
 	}
 }
 
 class TileGroup {
 	id: number;
+	title: string;
 	desc: string;
+	items: TileItem[];
 
-	constructor(id: number, desc: string) {
+	constructor(id: number, title: string, desc: string, items: TileItem[]) {
 		this.id = id;
+		this.title = title;
 		this.desc = desc;
+		this.items = items;
 	}
 }
