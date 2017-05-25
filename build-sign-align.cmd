02e2d064519439e31c3bf17b42888b37079386a8
cheekbyname <zenspideruk@googlemail.com>
2017-05-25 10:42:59 +0100

    Splash screen image

--- build-sign-align.cmd
+++ build-sign-align.cmd
@@ -1,6 +1,6 @@
 rem Remember to up android version number in config.xml
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore "c:\dev\active\platforms\android\build\outputs\apk\android-release-unsigned.apk" activekey
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore "c:\dev\active\platforms\android\build\outputs\apk\android-release-unsigned.apk" activekey -storepass "Disco2000"
 del BlackwoodActive.apk
 "c:\Program Files (x86)\Android\android-sdk\build-tools\23.0.1\zipalign.exe" -v 4 c:\dev\Active\platforms\android\build\outputs\apk\android-release-unsigned.apk BlackwoodActive.apk
 echo "APK Built, Signed & Aligned"
