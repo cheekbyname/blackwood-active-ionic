e82cc7c4567ab89e2f0b44539d7bdd5995abad2d
Alex Cheek <AlexC@blackwoodgroup.org.uk>
2017-08-23 20:31:47 +0100

    Alert for unimplemented Daily Notes function

--- src/pages/dailynotes.page/dailynotes.page.ts
+++ src/pages/dailynotes.page/dailynotes.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams, PopoverController, Events } from 'ionic-angular';
+import { AlertController, NavController, NavParams, PopoverController, Events } from 'ionic-angular';
 
 import { Client } from '../../models/client';
 import { DailyNote } from '../../models/dailynote';
@@ -11,9 +11,8 @@ import { AddDailyNotePage } from '../../pages/adddailynote.page/adddailynote.pag
 	templateUrl: 'dailynotes.page.html'
 })
 export class DailyNotesPage implements OnInit {
-	constructor(public clientService: ClientService, public navCtrl: NavController,
-		navParams: NavParams, public popCtrl: PopoverController,
-		public events: Events) {
+	constructor(public clientService: ClientService, public navCtrl: NavController, navParams: NavParams,
+		public popCtrl: PopoverController, public events: Events, public alerts: AlertController) {
 		this.client = navParams.get("client");
 	}
 
@@ -42,11 +41,11 @@ export class DailyNotesPage implements OnInit {
 
 	public formatDate(date: Date): string {
 		var dt = new Date(date.toString());
-		return dt.toLocaleDateString() + ' ' + dt.toLocaleTimeString().substr(0,5);
+		return dt.toLocaleDateString() + ' ' + dt.toLocaleTimeString().substr(0, 5);
 	}
 
 	public pageOlder(): void {
-		if(this.hasOlder) {	// An approximation of there being more data
+		if (this.hasOlder) {	// An approximation of there being more data
 			this.pageIndex++;
 			this.fetchNotes();
 		}
@@ -60,11 +59,22 @@ export class DailyNotesPage implements OnInit {
 	}
 
 	public addNewNote(): void {
-		this.navCtrl.push( AddDailyNotePage, {client: this.client});
+		this.navCtrl.push(AddDailyNotePage, { client: this.client });
 	}
 
 	public saveNewNote(note: DailyNote): void {
 		this.dailyNotes.unshift(note);
+		var alert = this.alerts.create({
+			message: "This function is not implemented yet. Your Daily Note has not been saved to CareSys.",
+			title: "Sorry", subTitle: "Function Not Implemented", buttons: [
+				{
+					text: "Ok", handler: () => {
+						alert.dismiss();
+					}
+				}
+			]
+		});
+		alert.present();
 		// TODO Call API with new Note data
 	}
 }
